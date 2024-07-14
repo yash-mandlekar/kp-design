3e7da6a7aea83038a0e16968aab930e4a2c639f6
yash <ymandlekar9826@gmail.com>
2024-07-14 17:56:47 +0530

    final commit

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
 var dark = document.querySelector(".dark");
+var main = document.querySelector(".main");
+var footer = document.querySelector("footer");
 var img = document.querySelectorAll(".img");
 var bulb = document.querySelector("#bulb");
+function invert_elem(elem) {
+  elem.style.filter = "invert(1) hue-rotate(180deg)";
+}
+function invert_none(elem) {
+  elem.style.filter = "none";
+}
 dark.addEventListener("click", function () {
-  console.log(dark.classList);
   if (dark.classList.value === "dark") {
-    document.body.style.filter = "invert(1) hue-rotate(180deg)";
+    invert_elem(main);
+    invert_elem(footer);
     img.forEach((e) => {
-      e.style.filter = "invert(1) hue-rotate(180deg)";
+      invert_elem(e);
     });
-    localStorage.setItem("theme", "white");
     bulb.src = "https://i.postimg.cc/6QyTynzr/bulb-on.png";
   } else {
-    document.body.style.filter = "none";
+    invert_none(main);
+    invert_none(footer);
     img.forEach((e) => {
-      e.style.filter = "none";
+      invert_none(e);
     });
-    localStorage.setItem("theme", "dark");
     bulb.src = "https://i.postimg.cc/KjK1wL3c/bulb-off.png";
   }
   dark.classList.toggle("dark");
 });
+// window.onload = () => {
+//   var theme = localStorage.getItem("theme");
+//   if (theme == "dark") {
+//     bulb.src = "https://i.postimg.cc/KjK1wL3c/bulb-off.png";
+//     main.style.filter = "none";
+//     img.forEach((e) => {
+//       e.style.filter = "none";
+//     });
+//     bulb.style.filter = "none";
+//   } else {
+//     bulb.src = "https://i.postimg.cc/6QyTynzr/bulb-on.png";
+//     main.style.filter = "invert(1) hue-rotate(180deg)";
+//     img.forEach((e) => {
+//       e.style.filter = "invert(1) hue-rotate(180deg)";
+//     });
+//     dark.classList.toggle("dark");
+//     bulb.style.filter = "invert(1) hue-rotate(180deg)";
 
-window.onload = () => {
-  var theme = localStorage.getItem("theme");
-  if (theme == "dark") {
-    bulb.src = "https://i.postimg.cc/KjK1wL3c/bulb-off.png";
-    document.body.style.filter = "none";
-    img.forEach((e) => {
-      e.style.filter = "none";
-    });
-    bulb.style.filter = "none";
-  } else {
-    bulb.src = "https://i.postimg.cc/6QyTynzr/bulb-on.png";
-    document.body.style.filter = "invert(1) hue-rotate(180deg)";
-    img.forEach((e) => {
-      e.style.filter = "invert(1) hue-rotate(180deg)";
-    });
-    dark.classList.toggle("dark");
-    bulb.style.filter = "invert(1) hue-rotate(180deg)";
-
-  }
-};
+//   }
+// };
